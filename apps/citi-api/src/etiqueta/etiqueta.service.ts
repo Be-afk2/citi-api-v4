99b93b4bbdd8f65fa84515fe2cc718dd971a4050
Be-afk2 <122316334+Be-afk2@users.noreply.github.com>
2025-03-05 20:49:54 -0300

    arreglo paguinador
    paginador de información para no tener todos los datos de golpe

--- apps/citi-api/src/etiqueta/etiqueta.service.ts
+++ apps/citi-api/src/etiqueta/etiqueta.service.ts
@@ -4,15 +4,30 @@ https://docs.nestjs.com/providers#services
 
 import { Injectable } from '@nestjs/common';
 import { PaguinadorDto } from './dto/paguinadorDto.dto';
+import { Etiquetas } from 'apps/citi-back/src/entities/etiquetas.entiy';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-@Injectable()
+@Injectable() 
 export class EtiquetaService {
 
+    constructor(
+        @InjectRepository(Etiquetas)
+        private PaisEtiquetas: Repository<Etiquetas>,
 
 
-    async GetEtiquetas(data: PaguinadorDto) {
+
+    ) {}
+
+    async GetEtiquetas(paguinador: PaguinadorDto) {
+
+        const [data, total] = await this.PaisEtiquetas.findAndCount({
+            skip: (paguinador.Paguina - 1) * paguinador.Cantidad,
+            take: paguinador.Cantidad,
+        });
         return data;
-
     }
 
+
+    
 }
