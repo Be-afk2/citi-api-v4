6ff9b7762c6ebf648b6b040e79d9970c0a5aaa5c
Be-afk2 <122316334+Be-afk2@users.noreply.github.com>
2025-03-21 20:17:18 -0300

    Update app.controller.ts

--- apps/citi-api/src/app.controller.ts
+++ apps/citi-api/src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   getHello() {
     return {
       Name : "Citi-api",
-      Version : "0.0.2",
+      Version : "0.0.8",
       Developers : [{
         name : "BE"
       }]
