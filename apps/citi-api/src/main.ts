c829163ec89f6a2e797dc2987daf091093d5797e
Be-afk2 <122316334+Be-afk2@users.noreply.github.com>
2025-03-22 14:18:33 -0300

    arreglo cors
    agregado cors y arreglo de vulnerabilidades que no había tratado

--- apps/citi-api/src/main.ts
+++ apps/citi-api/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as path from 'path';
 import * as serveStatic from 'serve-static';
+var cors = require('cors')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(cors())
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
