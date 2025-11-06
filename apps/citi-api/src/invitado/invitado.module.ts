c8d19cb7efae800de5e8c71c532fdcf757aa2dd2
Benjamin Torres Sandoval <122316334+Be-afk2@users.noreply.github.com>
2025-11-06 18:10:54 -0300

    Añadir creación de usuarios invitados y compatibilidad con JWT
    Se mejoró el servicio InvitadoService para crear o recuperar un usuario invitado predeterminado y devolver un token JWT. Se actualizó el módulo InvitadoModule para proporcionar AuthService, JwtStrategy y configurar JwtModule. Se agregó registro en AuthService para la generación de tokens.

--- apps/citi-api/src/invitado/invitado.module.ts
+++ apps/citi-api/src/invitado/invitado.module.ts
@@ -4,11 +4,16 @@ import { TipoUser } from 'apps/citi-back/src/entities/TipoUser.entity';
 import { User } from 'apps/citi-back/src/entities/user.entity';
 import { InvitadoController } from './invitado.controller';
 import { InvitadoService } from './invitado.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 /*
 https://docs.nestjs.com/modules
 */
 
 import { Module } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { JwtStrategy } from '../auth/jwt.strategy';
 
 @Module({
     imports: [
@@ -17,8 +22,16 @@ import { Module } from '@nestjs/common';
             TipoUser,
             Etiquetas
         ]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: '1y' },
+            }),
+        })
     ],
     controllers: [InvitadoController,],
-    providers: [InvitadoService,],
+    providers: [InvitadoService, AuthService, JwtStrategy],
 })
 export class InvitadoModule { }
--- apps/citi-api/src/invitado/invitado.service.ts
+++ apps/citi-api/src/invitado/invitado.service.ts
@@ -2,15 +2,61 @@
 https://docs.nestjs.com/providers#services
 */
 
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TipoUser } from 'apps/citi-back/src/entities/TipoUser.entity';
+import { User } from 'apps/citi-back/src/entities/user.entity';
+import { hash, compare } from 'bcrypt';
 
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../auth/dto/CreateUser.dto';
+import { AuthService } from '../auth/auth.service';
+const UserInvitado = {
+    nombre: "Usuario",
+    apellido: "invitado",
+    correo: "invitado@gmail.com",
+    pass: "invitado",
+}
 @Injectable()
 export class InvitadoService {
 
+    constructor(
+        @InjectRepository(User)
+        private UsersRepository: Repository<User>,
+
+        @InjectRepository(TipoUser)
+        private TipoUserRepository: Repository<TipoUser>,
+
+        private jwtService: JwtService,
+        private AuthService: AuthService
+    ) { }
 
 
 
-        async GetInvitado(){
-        
+
+    async GetInvitado() {
+
+        let user = await this.UsersRepository.findOneBy({ correo: UserInvitado.correo });
+        if (user) {
+            user = await this.AuthService.findUserById(user.id, false)
+            return {
+                user: await this.AuthService.findUserById(user.id, false),
+                token: await this.AuthService.get_token(user),
+            };
+        }
+
+        user = await this.UsersRepository.create();
+        user.nombre = UserInvitado.nombre;
+        user.apellido = UserInvitado.apellido;
+        user.correo = UserInvitado.correo;
+        user.password = await hash(UserInvitado.pass, 10);
+        user.tipoUser = await this.TipoUserRepository.findOneBy({ id: 3 });
+        await user.save();
+
+        return {
+            user: await this.AuthService.findUserById(user.id, false),
+            token: await this.AuthService.get_token(user),
+        };
     }
 }
