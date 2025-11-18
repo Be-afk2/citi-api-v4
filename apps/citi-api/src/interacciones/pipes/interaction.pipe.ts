aad01d4686e9aaaa613b74d6cdecea343dc62aa7
Zero0242 <pedrocorream66@gmail.com>
2025-11-18 10:42:45 -0300

    [PIPE]

--- apps/citi-api/src/interacciones/pipes/interaction.pipe.ts
+++ apps/citi-api/src/interacciones/pipes/interaction.pipe.ts
@@ -1,19 +1,30 @@
-import { Injectable, NotFoundException, PipeTransform } from '@nestjs/common';
-import { InteractionEnum } from '../interfaces/interaction.enum';
+import {
+  ArgumentMetadata,
+  Injectable,
+  NotFoundException,
+  PipeTransform,
+} from '@nestjs/common';
 
 @Injectable()
 export class ParseInteractionPipe implements PipeTransform {
+  // Mapeo de acciones a números o enums (según lo que use tu servicio)
+  private interactionTypes = {
+    like: 1,
+    compartir: 2,
+    visto: 3,
+  };
+
   /**
    * Parsea el valor de interacción a su enum correspondiente.
    * @param value deberia de ser el param del url
    * @returns Enum InteractionEnum correspondiente
    */
   transform(value: any) {
-    const result = InteractionEnum[value];
+    const result = this.interactionTypes[value];
 
     if (!result) {
       throw new NotFoundException(
-        `Interaction not supported, you should use one of the following: [ ${Object.keys(InteractionEnum)} ]`,
+        `Interaction not supported, you should use one of the following: [ ${Object.keys(this.interactionTypes)} ]`,
       );
     }
     return result;
