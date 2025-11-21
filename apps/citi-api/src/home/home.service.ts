7a63268a4c8564eaf1239839234789a7c3463fa5
Benjamin Torres Sandoval <122316334+Be-afk2@users.noreply.github.com>
2025-11-21 18:48:56 -0300

    Update home.service.ts

--- apps/citi-api/src/home/home.service.ts
+++ apps/citi-api/src/home/home.service.ts
@@ -112,7 +112,7 @@ export class HomeService {
       )
       .addSelect((subQuery) => {
         return subQuery
-          .select('Fotos.path')
+          .select('Fotos.Path')
           .from(FotosLocal, 'fotos')
           .where('Fotos.localId = Local.id')
           .orderBy('RAND()')
